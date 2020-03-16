36d9f71a320958d20163f9765b1815a88d9c3524
Simon Gray <simongray@gmail.com>
2020-03-16 13:07:06 +0100

    WIP: apache2 web server

--- repository.clarin.dk/start.sh
+++ repository.clarin.dk/start.sh
@@ -3,7 +3,9 @@
 # TODO: not working properly, https://stackoverflow.com/questions/31645550/why-psql-cant-connect-to-server
 sudo -u postgres /usr/lib/postgresql/10/bin/initdb -D /usr/local/pgsql/data &&
 sudo -u postgres /usr/lib/postgresql/10/bin/pg_ctl -D /usr/local/pgsql/data -l /var/log/postgresql/logfile start &&
+apache2ctl start && 
 /opt/tomcat/bin/startup.sh &&
-touch services-started
+touch services-started &&
+ping localhost
 
 # TODO: apache dep is missing + apache integration with tomcat
