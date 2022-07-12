3b991bde3609298c1865a6325c69d34c26ec3351
Philip Diderichsen <philip.diderichsen@gmail.com>
2022-07-12 15:00:48 +0200

    LANCHART: Render audio in partitur service.

--- setups/lanchart/partitur/start.sh
+++ setups/lanchart/partitur/start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-echo "Hellooo"
-cd /opt/lanchart_partitur
+cd /opt/partitur/lanchart_partitur
 pip3 install -r requirements.txt
-cp config/config_docker.py config/config.py
-# Note: The path below must NO LONGER.. be the one set to point at $CODE_PATH in docker-compose.yml.
-python3 /opt/lanchart_partitur/partitur_app.py
-#sleep infinity
+FLASK_APP=partitur_app.py FLASK_CONFIG=prod flask run --host 0.0.0.0
+
