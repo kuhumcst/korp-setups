682e54d3ae699b5b07115b2a8af3d48220ac7e96
Philip Diderichsen <philip.diderichsen@gmail.com>
2023-07-11 08:46:52 +0200

    Switched to httpx. Slightly more stable. Datasets up to 20000 rows tested. Takes 10 mins ..

--- setups/lancharttest/exporter/app.py
+++ setups/lancharttest/exporter/app.py
@@ -1,12 +1,14 @@
+import os
+import httpx
 import json
 import datetime
 import logging
-import requests
 import urllib.parse as urlp
 from flask import Flask, request, Response
 import korpexport.exporter as ke
 import tempfile
 app = Flask(__name__)
+
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
 
 #QUERY_ENDPOINT = 'https://alf.hum.ku.dk/korp/backend/query?'
@@ -18,109 +20,6 @@ QUERY_ENDPOINT = 'http://backend:1234/query?'
 Test URL:
 http://localhost:5000/download/csv?default_context=1%20sentence&show=sentence%2Cpos%2Cmsd%2Clemma%2Cref%2Cprefix%2Csuffix&show_struct=text_title&start=0&end=24&corpus=LSPCLIMATEAKTUELNATURVIDENSKAB%2CLSPCLIMATEDMU%2CLSPCLIMATEHOVEDLAND%2CLSPCLIMATEOEKRAAD&cqp=%5Bword%20%3D%20%22mange%22%5D&query_data=&context=&incremental=true&default_within=sentence&within=
 """
-
-
-class CustomEncoder(json.JSONEncoder):
-    """Custom json encoder that will encode bytes to utf8 strings when necessary."""
-    def default(self, obj):
-        if isinstance(obj, bytes):
-            return obj.decode('utf-8')  # Convert bytes to string
-        return super().default(obj)
-
-
-def add_headers(resp, result):
-    """Add HTTP headers to the downloadable file resp. Adapted from Jyrki.
-
-    Arguments:
-        resp (resp): The resp to which to add headers.
-        result (dict): The downloadable file contents and information
-            about it (or an error message); may contain the following
-            keys that affect the output headers:
-
-            - download_content_type => Content-Type (default:
-              ``text/plain``)
-            - download_charset => Charset (default: utf-8)
-            - download_filename => Content-Disposition filename
-            - download_content => Length of the content to
-              Content-Length
-
-            If `result` contains the key ``ERROR``, just add
-            the status code 502 and no other headers.
-    """
-    if "ERROR" in result:
-        resp.status_code = 502
-    else:
-        charset = result.get("download_charset", "utf-8")
-        content_type = result.get("download_content_type", "text/plain")
-        download_filename = result.get("download_filename", "korp_kwic")
-        content_type_header = f'Content-Type: {content_type}'
-        if charset:
-            content_type_header = f'{content_type_header}; charset={charset}'
-        resp.headers['Content-Type'] = content_type_header  # 'text/csv; charset=utf-8'
-        resp.headers['Content-Disposition'] = make_content_disposition_attachment(download_filename)
-        resp.headers['Content-Length'] = str(len(result["download_content"]))
-    return resp
-
-
-def make_content_disposition_attachment(filename):
-    """Make a HTTP Content-Disposition header with attachment filename. Adapted from Jyrki.
-
-    Arguments:
-        filename (str): The file name to use for the attachment
-
-    Returns:
-        str: A HTTP ``Content-Disposition`` header for an attachment
-            with a parameter `filename` with a value `filename`
-
-    If `filename` contains non-ASCII characters, encode it in UTF-8 as
-    specified in RFC 5987 to the `Content-Disposition` header
-    parameter `filename*`, as showin in a `Stackoverflow discussion`_.
-    For a wider browser support, also provide a `filename` parameter
-    with the encoded filename. According to the discussion, this does
-    not work with IE prior to version 9 and Android browsers.
-    Moreover, at least Firefox 28 on Linux seems save an empty file
-    with the corresponding Latin-1 character in its name, in addition
-    to the real file.
-
-    .. _Stackoverflow discussion: http://stackoverflow.com/questions/93551/how-to-encode-the-filename-parameter-of-content-disposition-header-in-http
-    """
-    filename = urlp.quote(filename)
-    return (("Content-Disposition: attachment; "
-             + ("filename*=UTF-8''{filename}; " if "%" in filename else "")
-             + "filename={filename}")
-            .format(filename=filename))
-
-
-def add_download_headers_and_content(resp, form):
-    """Get the downloadable content using Jyrki's exporter."""
-    result = ke.make_download_file(form, form.get("korp_server", "KORP_SERVER"))
-    result_content_with_bom = get_csv_content_with_bom(form)
-    resp = add_headers(resp, result)
-    resp.data = result_content_with_bom
-    return resp
-
-
-def get_csv_content_with_bom(form, skip_header=False):
-    result = ke.make_download_file(form, form.get("korp_server", "KORP_SERVER"))
-    result_charset = result.get("download_charset", "utf-8")
-    print('result headers: ', list(result))
-    result_content_bytes = result.get("download_content", "### No content :( ###")
-    # Prepending the BOM '\uFEFF', creating a file encoded in UTF-8-BOM, makes
-    # e.g. csv readable in Excel as well as text editors on Windows as well as Mac.
-    result_content = result_content_bytes.decode(result_charset, errors='replace')
-    if skip_header:
-        result_content = '\n'.join(result_content.splitlines()[13:]) + '\n'
-    result_content_with_bom = '\uFEFF' + result_content
-    return result_content_with_bom
-
-
-def generate_output_stream(filename):
-    with open(filename, 'rb') as f:
-        while True:
-            chunk = f.read(4096)  # Read in chunks of 4096 bytes
-            if not chunk:
-                break
-            yield chunk
 
 
 @app.route('/download/<content_type>')
@@ -146,14 +45,14 @@ def download(content_type):
     korp_hits_display = int(query_params.get('hits_display', ['0'])[0])  # Total hits according to Korp search.
     app.logger.info('korp_hits_display: ' + str(korp_hits_display))
     max_rows = 500000  # Our imposed row download limit.
-    chunk_size = 500
+    chunk_size = 50
     start_arg = 0
+    client = httpx.Client(timeout=600.0)
 
-    # Trying to handle big file sizes ... It is kind of assumed here that /tmp in Docker will be
-    # mapped to a location on the host with plenty of space.
-    tf = tempfile.NamedTemporaryFile(delete=False, dir='/tmp')
-    with open(tf.name, 'w') as temp_file:  # tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_file:
+    ftf = tempfile.NamedTemporaryFile(delete=False, dir='/tmp')
+    with open(ftf.name, 'w') as formatted_temp_file:
 
+        # TODO Why does the while loop start over with start_arg = 0 if there is an error in line 237?
         while start_arg <= korp_hits_display or start_arg < max_rows:
             app.logger.info('start_arg: ' + str(start_arg))
             if start_arg > max_rows:
@@ -163,34 +62,100 @@ def download(content_type):
             elif start_arg > korp_hits_display:
                 app.logger.info('Greater than korp_hits_display! Breaking before further reading or writing.')
                 break
+
+            # Build URL to get data with the current start_arg and end_arg.
             query_params['start'] = [str(start_arg)]
             query_params['end'] = [str(start_arg + chunk_size - 1)]
             url = QUERY_ENDPOINT + urlp.urlencode(query_params, doseq=True)
-            api_response = requests.get(url, timeout=1200)
-            api_response_content = api_response.content.decode('utf8')
-            app.logger.info('api_response_content: ' + api_response_content[:200])
+            app.logger.info('url: ' + url)
 
-            try:
-                form = {"query_result": api_response_content, "format": content_type}
-                if start_arg == 0:
-                    result_content_with_bom = get_csv_content_with_bom(form)
-                else:
-                    result_content_with_bom = get_csv_content_with_bom(form, skip_header=True)
-                # print('result_content_with_bom:\n', result_content_with_bom)
-                temp_file.write(result_content_with_bom)
-                start_arg += chunk_size
-            except ke.KorpExportError as e:
-                print(f'<H1>422 Unprocessable Content</H1>{str(e)}')
+            # Get the data from the backend. Retrieve the name of the temp file written to.
+            tf_name = stream_backend_query_to_tempfile(client, url)
+
+            # Transform the data using Jyrki's code. Write it to formatted_temp_file.
+            result_content_with_bom = make_formatted_data(tf_name, content_type, start_arg)
+            formatted_temp_file.write(result_content_with_bom)
+            start_arg += chunk_size
+
+    rsp = Response(generate_output_stream(ftf.name), mimetype='text/csv')
+    rsp.headers.set('Content-Disposition', 'attachment',
+                    filename='korp_kwic_' + datetime.datetime.now().isoformat() + '.csv')
+    return rsp
+
+
+def stream_backend_query_to_tempfile(client, url):
+    """Use httpx client to get data from the Korp backend in a chunked way. Write to tempfile.
+    Returns the name of the tempfile written to."""
+    # Trying to handle big file sizes ... It is kind of assumed here that /tmp in Docker will be
+    # mapped to a location on the host with plenty of space.
+    tf = tempfile.NamedTemporaryFile(delete=False, dir='/tmp')
+    with tf as temp_file:  # tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_file:
+        # TODO Med for stor en chunk_size ser det ud til at risikoen for følgende fejl er større:
+        #  httpx.RemoteProtocolError: peer closed connection without sending complete message body (incomplete chunked read).
+        #  Problemet er at man ikke kan vide på forhånd hvor tung den enkelte konkordanslinje er
+        #  da der kan være forskelligt antal opmærkninger afhængigt af korpus.
+        #  Så chunk_size vil formentlig være lidt lille i mange tilfælde hvis den skal være lille nok
+        #  til at at korpusser med meget tunge konkordanslinjer skal kunne downloades ...
+        # Total timeout for entire request. Timeouts set to None are governed by the total timeout.
+        timeout = httpx.Timeout(300.0, pool=None, connect=None, read=None, write=None)
+        with client.stream("GET", url, timeout=timeout) as response:
+            chunk_no = 1
+            for chunk in response.iter_bytes(chunk_size=1000000):  # Get 1MB of data at a time
+                app.logger.info(f'Writing chunk {chunk_no} to "{temp_file.name}"..')
+                temp_file.write(chunk)
+                chunk_no += 1
+        tf.flush()  # Empty Python's internal buffers to the operating system.
+        os.fsync(tf.fileno())  # Ensure the operating system's buffers are written to disk.
+        return tf.name
+
+
+def make_formatted_data(tf_name, content_type, start_arg):
+    with open(tf_name, 'rb') as saved_tempfile:
+        saved_tempfile_content = saved_tempfile.read()
+    form = {"query_result": saved_tempfile_content, "format": content_type}
+
+    try:
+        if start_arg == 0:
+            result_content_with_bom = get_csv_content_with_bom(form)
+        else:
+            result_content_with_bom = get_csv_content_with_bom(form, skip_header=True)
+    except json.JSONDecodeError as e:
+        print(f"JSONDecodeError: {e}")
+        error_position = e.pos  # Position where the error occurred
+        window = 40  # We will print 40 characters before and after the error position
+        # TODO Handle JSON error properly ...?
+        print(f"JSON around error position: "
+              f"{saved_tempfile_content[max(0, error_position - window):error_position + window]}\n\n\n\n\n\n\n\n\n\n")
+        return 'Blaha JSONDecodeError :('
+
+    return result_content_with_bom
+
+
+def get_csv_content_with_bom(form, skip_header=False):
+    result = ke.make_download_file(form, form.get("korp_server", "KORP_SERVER"))
+    result_charset = result.get("download_charset", "utf-8")
+    app.logger.info('get_csv_content_with_bom: result headers: ' + str(list(result)))
+    result_content_bytes = result.get("download_content", "### No content :( ###")
+    app.logger.info('get_csv_content_with_bom: result_content_bytes len: ' + str(len(result_content_bytes)))
+    # Prepending the BOM '\uFEFF', creating a file encoded in UTF-8-BOM, makes
+    # e.g. csv readable in Excel as well as text editors on Windows as well as Mac.
+    result_content = result_content_bytes.decode(result_charset, errors='replace')
+    if skip_header:
+        result_content = '\n'.join(result_content.splitlines()[13:]) + '\n'
+    result_content_with_bom = '\uFEFF' + result_content
+    app.logger.info('get_csv_content_with_bom: result_content_with_bom len: ' + str(len(result_content_with_bom)))
+    return result_content_with_bom
+
+
+def generate_output_stream(filename):
+    with open(filename, 'rb') as f:
+        while True:
+            chunk = f.read(4096)  # Read in chunks of 4096 bytes
+            app.logger.info('generate_output_stream: chunk head: ' + str(chunk)[:50])
+            app.logger.info('generate_output_stream: chunk len: ' + str(len(str(chunk))))
+            if not chunk:
                 break
-
-    #if '"ERROR":' in api_response_content:
-    #    response.status_code = 502
-    #response.data = f'<H1>422 Unprocessable Content</H1>{str(e)}'
-    #response.status_code = 422
-    response = Response(generate_output_stream(temp_file.name), mimetype='text/csv')
-    response.headers.set('Content-Disposition', 'attachment',
-                         filename='korp_kwic_' + datetime.datetime.now().isoformat() + '.csv')
-    return response
+            yield chunk
 
 
 if __name__ == '__main__':
