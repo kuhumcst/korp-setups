b20c9ecca0180de74deb264fc1ad8a44dc0af19c
Philip Diderichsen <cpd@hum.ku.dk>
2023-09-21 14:32:10 +0200

    Add download countdown; refactor.

--- setups/lancharttest/exporter/static/filedownload.js
+++ setups/lancharttest/exporter/static/filedownload.js
@@ -5,10 +5,7 @@
 function downloadButtonClick(socket) {
     // Function that runs when the download button is clicked.
     var uniqueId = generateUniqueId();
-    console.log('uniqueId: ' + uniqueId);
-
     // Initiate the download by calling the /getfile endpoint
-    //fetch('/getfile2/csv?uid=' + uniqueId)
     fetch('/getfile/csv' + window.location.search + '&uid=' + uniqueId)
         .then(response => response.json())
         .then(data => {
@@ -29,49 +26,106 @@ function generateUniqueId() {
 
 function trackProgress(socket, uniqueId) {
     // Function to track progress and initiate the download once it's ready
-    console.log('Tracking progress for unique ID: ' + uniqueId);
-    // Show the progress bar
-    document.getElementById('progress-container').style.display = 'flex';
-    // Set up an interval to periodically check the download progress
-    var progressInterval = setInterval(function() {
-        console.log('Emitting get_status ..');
-        socket.emit('get_status', uniqueId);
-    }, 100); // Adjust the interval as needed
-
+    document.getElementById('progress-container').style.display = 'flex';  // Show progress bar.
+    let startTime = new Date();
+    let elapsedTime = 0;
+    let percentageToTimes = {};
+    let progressRates = [];
+    let progressInterval = emitStatus(socket, uniqueId);  // Emit progress status and keep a handle on the interval.
     // Listen for status updates from the server
     socket.on('status', function(data) {
-        var uid = data.uid;
-        console.log('Uid: ' + uid);
         // For the correct uid, update progress bar and percentage.
-        if (uid === uniqueId) {
-            updateProgress(uid, data, progressInterval, socket);
+        if (uniqueId === data.uid) {
+            updateProgress(uniqueId, data, progressInterval, socket);
+            elapsedTime = (new Date() - startTime) / 1000;  // Elapsed time in seconds.
+            // Add an empty array when a new percentage is first encountered, then add the time value.
+            if (!percentageToTimes[data.progress]) {
+                percentageToTimes[data.progress] = [];
+            }
+            percentageToTimes[data.progress].push(elapsedTime);
+            // Iterate over all but the last array of times (as the last one is assumed to be incomplete).
+            for (let progressPercentage of Object.keys(percentageToTimes).slice(0, -1)) {
+                progressRate = getProgressRate(progressPercentage, percentageToTimes);
+                if (!progressRates.includes(progressRate)) {  // Add to overall array of progress rates.
+                    progressRates.push(progressRate);
+                }
+            }
+            // Calculate estimated time left - when there are enough data to skip the first, skewed data.
+            if (progressRates.length > 3) {
+                // Calculate a current average progress rate based on the last n observations.
+                timeRemainingString = getTimeRemainingString(elapsedTime, progressRates);
+                document.getElementById('timer-val').innerText = timeRemainingString;
+                if (data.progress == 100) {
+                    document.getElementById('timer-val').innerText = "00:00:00";
+                }
+            }
         }
     });
-
     // Handle server aborts
     socket.on('abort', function(data) {
         socket.disconnect();
         document.getElementById('warnings').innerHTML = '<div id="warning">' + data.reason + '</div>';
     });
+}
 
+
+function emitStatus(socket, uniqueId) {
+    // Set up an interval to periodically check the download progress.
+    let progressInterval = setInterval(function() {
+        socket.emit('get_status', uniqueId);
+    }, 1000); // Emit status every 1000 ms.
+    return progressInterval
+}
+
+
+function getProgressRate(progressPercentage, percentageToTimes) {
+    // Calculate a progress rate from total (max) elapsed time for the given progress percentage.
+    let timeArray = percentageToTimes[progressPercentage];
+    let maxTime = Math.max(...timeArray);
+    let progressRate = progressPercentage / maxTime;
+    return progressRate;
+}
+
+
+function getTimeRemainingString(elapsedTime, progressRates) {
+    // Get remaining download time based on elapsed time and latest progress rates seen.
+    let latestRates = progressRates.slice(-3);  // n latest progress rates.
+    let latestSum = latestRates.reduce((accumulator, currValue) => accumulator + currValue, 0);
+    let currentProgressRate = latestSum / latestRates.length;
+    let estimatedProgress = elapsedTime * currentProgressRate;
+    let estimatedProgressLeft = 100 - estimatedProgress;
+    let estimatedTimeLeft = estimatedProgressLeft / currentProgressRate;
+    let timeRemainingString = formatTime(estimatedTimeLeft);
+    return timeRemainingString;
+}
+
+
+function formatTime(secondsTimeFloat) {
+    // Format time in 00:00:00 format.
+    let secondsTime = Math.floor(secondsTimeFloat);
+    let hours = Math.floor(secondsTime / 3600);
+    secondsTime %= 3600;
+    let minutes = Math.floor(secondsTime / 60);
+    let seconds = secondsTime % 60;
+    const formattedSeconds = String(seconds).padStart(2, '0');
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedHours = String(hours).padStart(2, '0');
+    let timeString = `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+    return timeString;
 }
 
 
 function updateProgress(uid, data, progressInterval, socket) {
     // Do the actual updating of the progress bar - and trigger the actual download when reaching 100%
-    console.log('Correct uid: ' + uid);
     var progress = data.progress;
-    console.log('Progress: ' + progress);
     document.getElementById('progress-val').innerText = progress + '%';
     document.getElementById('progress-bar').value = progress;
-
-
     // If the download is complete, clear the interval and initiate the actual download
     if (progress === 100) {
         clearInterval(progressInterval);
         socket.disconnect();
         console.log('Socket disconnected');
-        document.getElementById('label-small').innerText = 'Færdig.'
+        document.getElementById('download-label').innerText = 'Færdig.';
         // Download file when ready
         fileDownload(uid);
     }
@@ -81,10 +135,7 @@ function updateProgress(uid, data, progressInterval, socket) {
 function fileDownload(uniqueId) {
     // Do the actual file download
     setTimeout(function() {
-        // var downloadUrl = '/getfile2/csv' + window.location.search + '&uid=' + uniqueId;
-        var downloadUrl = '/download2/' + uniqueId;
-        console.log('downloadUrl: ' + downloadUrl);
-
+        let downloadUrl = '/download2/' + uniqueId;
         // Create invisible link and "click" it to initiate download
         var link = document.createElement('a');
         link.href = downloadUrl;
